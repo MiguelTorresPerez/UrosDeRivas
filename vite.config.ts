9c88450e73ed86ea82fc8844b1517b2b06f84c9f
MiguelTorresPerez <miguelsiok@hotmail.com>
2026-03-25 05:07:13 +0100

    chore: configuration for GH Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // For correct resolution in GitHub Pages
+  base: '/UrosDeRivas/', // For correct resolution in GitHub Pages
 })
